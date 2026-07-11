e18d6dfe92d42c7b4bc9966dd2a206a45fe73ab6
szk49109 <dai2sebire5m0i4r7p@gmail.com>
2026-07-11 16:44:01 +0900

    feat: カウンターの実装

--- html/js/script.js
+++ html/js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+    counter.addEventListener("click", (e) => {
+        const isPlus = e.target.classList.contains("plus");
+        const isMinus = e.target.classList.contains("minus");
+
+        if (isPlus || isMinus) {
+            const value = counter.querySelector(".value");
+            let currentValue = parseInt(value.textContent);
+
+            if (isPlus) {
+                currentValue++;
+            } else if (isMinus) {
+                currentValue--;
+            }
+
+            value.textContent = currentValue;
+        }
+    });
+});
