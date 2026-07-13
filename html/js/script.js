161151e68af3c9fe1c466cab32441fe36dae78e5
szk49109 <dai2sebire5m0i4r7p@gmail.com>
2026-07-13 18:48:35 +0900

    feat: カウンターのリセットボタンを追加

--- html/js/script.js
+++ html/js/script.js
@@ -6,8 +6,9 @@ counters.forEach((counter) => {
     counter.addEventListener("click", (e) => {
         const isPlus = e.target.classList.contains("plus");
         const isMinus = e.target.classList.contains("minus");
+        const isReset = e.target.classList.contains("reset");
 
-        if (isPlus || isMinus) {
+        if (isPlus || isMinus || isReset) {
             const display = counter.querySelector(".display");
             let currentValue = parseInt(display.textContent);
 
@@ -15,6 +16,8 @@ counters.forEach((counter) => {
                 currentValue++;
             } else if (isMinus) {
                 currentValue--;
+            } else if (isReset) {
+                currentValue = 0;
             }
 
             display.textContent = currentValue;
