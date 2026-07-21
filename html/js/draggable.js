9a03f5556911b47b81b30e368e4a110c175f630f
NS-natsu <t87728@gmail.com>
2026-07-22 05:42:53 +0900

    feat: ドラッグ中はリリース時のカウンター設置位置に影を出すようにする

--- html/js/draggable.js
+++ html/js/draggable.js
@@ -6,7 +6,7 @@
  */
 function onDragStart(event) {
   const container = event.currentTarget;
-  if (!container || !event.target?.matches?.("div.dragger")) {
+  if (!container || !event.target?.matches?.(".dragger")) {
     return;
   }
 
@@ -14,15 +14,32 @@ function onDragStart(event) {
   container.removeEventListener(event.type, onDragStart);
 
   const dragTarget = event.target.closest(".counter");
+  const placeholder = container.querySelector(".placeholder");
+  const anchorNode = container.querySelector("#add-counter");
+
+  const otherCounters = Array.from(container.querySelectorAll(".counter"));
+  let insertOrder = otherCounters.indexOf(dragTarget);
+  otherCounters.splice(insertOrder, 1);
 
   // 移動可能なことがわかるように少しずらす
   dragTarget.style.left = `${dragTarget.offsetLeft - 5}px`;
   dragTarget.style.top = `${dragTarget.offsetTop - 5}px`;
   dragTarget.toggleAttribute('data-dragging', true);
 
+  otherCounters.forEach((item, idx) => {
+    item.style.order = idx;
+  });
+  placeholder.style.order = insertOrder;
+  anchorNode.style.order = otherCounters.length;
+
   const onPointerMove = ({ movementX, movementY }) => {
     dragTarget.style.top = `${dragTarget.offsetTop + movementY}px`;
     dragTarget.style.left = `${dragTarget.offsetLeft + movementX}px`;
+
+    const delta = getSlotDelta(dragTarget, placeholder, container);
+    insertOrder = clampNumber(insertOrder + delta, 0, otherCounters.length);
+
+    placeholder.style.order = insertOrder;
   };
 
   const onDragEnd = ({ type: eventType }) => {
@@ -30,8 +47,14 @@ function onDragStart(event) {
     window.removeEventListener("pointercancel", onDragEnd);
     window.removeEventListener("pointerup", onDragEnd);
 
+    if (eventType === "pointerup") {
+      container.insertBefore(dragTarget, otherCounters[insertOrder] ?? anchorNode);
+    }
+
+    for (const elm of container.children) {
+      elm.removeAttribute("style");
+    }
     dragTarget.toggleAttribute('data-dragging', false);
-    dragTarget.removeAttribute("style");
     dragTarget.focus({ focusVisible: true });
 
     container.addEventListener(event.type, onDragStart);
@@ -41,4 +64,49 @@ function onDragStart(event) {
   window.addEventListener("pointerup", onDragEnd, { once: true });
   window.addEventListener("pointercancel", onDragEnd, { once: true });
   window.addEventListener("pointermove", onPointerMove);
+}
+
+/**
+ * @param {number} value
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function clampNumber(value, min = -Infinity, max = Infinity) {
+  return Math.min(max, Math.max(min, value));
+}
+
+/**
+ * @param {HTMLDivElement} dragElm
+ * @param {HTMLDivElement} baseElm
+ * @param {HTMLDivElement} container
+ * @returns {number}
+ */
+function getSlotDelta(dragElm, baseElm, container) {
+  const containerStyle = getComputedStyle(container);
+
+  const rowGap = parseFloat(containerStyle.rowGap) || 0;
+  const columnGap = parseFloat(containerStyle.columnGap) || 0;
+
+  const slotWidth = baseElm.offsetWidth + columnGap;
+  const slotHeight = baseElm.offsetHeight + rowGap;
+
+  const maxColumns = Math.max(1, Math.floor((container.offsetWidth + columnGap) / slotWidth));
+  const maxRows = Math.max(1, Math.floor((container.offsetHeight + rowGap) / slotHeight));
+
+  const baseRow = Math.floor(baseElm.offsetTop / slotHeight);
+  const baseColumn = Math.floor(baseElm.offsetLeft / slotWidth);
+
+  const columnDelta = clampNumber(
+    Math.round((dragElm.offsetLeft - baseElm.offsetLeft) / slotWidth),
+    -baseColumn,
+    maxColumns - baseColumn - 1
+  );
+  const rowDelta = clampNumber(
+    Math.round((dragElm.offsetTop - baseElm.offsetTop) / slotHeight),
+    -baseRow,
+    maxRows - baseRow - 1
+  );
+
+  return maxColumns * rowDelta + columnDelta;
 }
